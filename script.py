f726d31fffeb0b3436ebf036759c7f4d5e317faa
Kartik <kartik.c.kulloli@gmail.com>
2023-04-15 17:21:37 +0530

    v1.1

--- script.py
+++ script.py
@@ -32,8 +32,9 @@ reddit = praw.Reddit(
 cache = {}
 
 # Subreddit and trigger phrase
-subreddit_name = 'flatapartmentcheck'
-trigger_phrase = '!wholesomenesscheck'
+subreddit_names = ['flatapartmentcheck',
+                   'indianteenagers', 'jeeneetards', 'twoxindia']
+trigger_phrases = ['!wholesomenesscheck', '!wholesomecheck', '!uwucheck']
 
 # Bot username
 bot_username = 'wholesome-counter'
@@ -45,56 +46,60 @@ try:
 except (ValueError, FileNotFoundError):
     times_called = 0
 
-# Listen for comments in subreddit
-for incoming_comment in reddit.subreddit(subreddit_name).stream.comments(skip_existing=True):
-    # Check if comment contains trigger phrase
-    if trigger_phrase in incoming_comment.body:
-        # Get parent comment and author name
-        parent_comment = incoming_comment.parent()
-        user_name = parent_comment.author.name
+# Listen for comments in subreddits
+comments_to_reply = []
+for subreddit_name in subreddit_names:
+    subreddit = reddit.subreddit(subreddit_name)
+    for incoming_comment in subreddit.stream.comments(skip_existing=True):
+        # Check if comment contains any of the trigger phrases
+        if any(phrase in incoming_comment.body for phrase in trigger_phrases):
+            # Get parent comment and author name
+            parent_comment = incoming_comment.parent()
+            user_name = parent_comment.author.name
 
-        # Check if bot is being called
-        if user_name.lower() == bot_username.lower():
-            # Construct reply text with total times bot was called
-            reply_text = f'The wholesome counter bot has been called {times_called+1} times till date.'
+            # Check if bot is being called
+            if user_name.lower() == bot_username.lower():
+                # Construct reply text with total times bot was called
+                reply_text = f'The wholesome counter bot has been called {times_called+1} times till date.'
 
-            # Reply to comment
-            incoming_comment.reply(reply_text)
+                # Add comment to list of comments to reply to
+                comments_to_reply.append((incoming_comment, reply_text))
 
-        else:
-            # Get top 1,000 comments of user
-            # Check if cached response exists for user
-            # 604800 seconds = 1 week
-            if user_name in cache and 'timestamp' in cache[user_name] and time.time() - cache[user_name]['timestamp'] <= 604800:
-                api_comments = cache[user_name]['response']
             else:
-                # Make API request for user comments
-                url = f'https://api.pushshift.io/reddit/comment/search?html_decode=true&after=0&author={user_name}&size=500'
-                with requests.Session() as session:
-                    response = session.get(url)
-                api_comments = response.json()['data']
+                # Get top 1,000 comments of user
+                # Check if cached response exists for user
+                # 604800 seconds = 1 week
+                if user_name in cache and 'timestamp' in cache[user_name] and time.time() - cache[user_name]['timestamp'] <= 604800:
+                    api_comments = cache[user_name]['response']
+                else:
+                    # Make API request for user comments
+                    url = f'https://api.pushshift.io/reddit/comment/search?html_decode=true&after=0&author={user_name}&size=500'
+                    with requests.Session() as session:
+                        response = session.get(url)
+                    api_comments = response.json()['data']
 
-                # Store response in cache
-                cache[user_name] = {
-                    'response': api_comments,
-                    'timestamp': time.time()
-                }
+                    # Store response in cache
+                    cache[user_name] = {
+                        'response': api_comments,
+                        'timestamp': time.time()
+                    }
 
-            # Initialize wholesome count
-            wholesome_count = 0
-            word_count = collections.Counter()
+                # Initialize wholesome count
+                wholesome_count = 0
+                word_count = collections.Counter()
 
-            # Analyze comments for wholesome words
-            for comment in api_comments:
-                # Clean and tokenize comment
-                comment_text = re.sub(r'[^\w\s]', '', comment['body']).lower()
-                comment_tokens = comment_text.split()
+                # Analyze comments for wholesome words
+                for comment in api_comments:
+                    # Clean and tokenize comment
+                    comment_text = re.sub(
+                        r'[^\w\s]', '', comment['body']).lower()
+                    comment_tokens = comment_text.split()
 
-                # Count wholesome occurrences
-                for token in comment_tokens:
-                    if token in wholesome_set:
-                        wholesome_count += 1
-                        word_count[token] += 1
+                    # Count wholesome occurrences
+                    for token in comment_tokens:
+                        if token in wholesome_set:
+                            wholesome_count += 1
+                            word_count[token] += 1
 
             # Construct table of wholesome words and counts
             table_rows = [
