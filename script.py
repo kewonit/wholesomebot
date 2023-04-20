4f3d57d9d397c7e3503190874b8c78bda7971e8b
Kartik <kartik.c.kulloli@gmail.com>
2023-04-21 03:20:25 +0530

    migrating to reddit api

--- script.py
+++ script.py
@@ -1,11 +1,9 @@
 import praw
 import os
-from dotenv import load_dotenv
-import requests
 import re
+from dotenv import load_dotenv
 import collections
 import time
-import json
 
 # Open the text file containing the words
 with open('wholesomewords.txt', 'r') as f:
@@ -43,8 +41,7 @@ bot_username = 'wholesome-counter'
 # Listen for comments in subreddits
 comments_to_reply = []
 for subreddit_name in subreddit_names:
-    subreddit = reddit.subreddit(
-        "flatapartmentcheck+IndianTeenagers+JEENEETards")
+    subreddit = reddit.subreddit("flatapartmentcheck+IndianTeenagers+JEENEETards")
     for incoming_comment in subreddit.stream.comments(skip_existing=True):
         # Check if comment contains any of the trigger phrases
         if any(phrase in incoming_comment.body.lower() for phrase in trigger_phrases):
@@ -68,32 +65,25 @@ for subreddit_name in subreddit_names:
                 comments_to_reply.append((incoming_comment, reply_text))
 
             else:
-                # Get top 500 comments of user
+                # Get top 300 comments of user
                 # Check if cached response exists for user
                 # 604800 seconds = 1 week
                 if user_name in cache and 'timestamp' in cache[user_name] and time.time() - cache[user_name]['timestamp'] <= 604800:
                     api_comments = cache[user_name]['response']
                 else:
                     # Make API request for user comments
-                    url = f'https://api.pushshift.io/reddit/comment/search?author={user_name}&size=250'
-                    retries = 4
+                    retries = 3
                     for i in range(retries):
                         try:
-                            with requests.Session() as session:
-                                response = session.get(url)
-                                # Add a sleep of 7 seconds after each API request
-                                time.sleep(7)
-                            api_comments = response.json()['data']
+                            api_comments = reddit.redditor(user_name).comments.new(limit=300)
+                            api_comments = [comment for comment in api_comments]
                             break
-                        # Add 3 retries
-                        except (requests.exceptions.RequestException, json.JSONDecodeError) as e:
+                        except praw.exceptions.PRAWException as e:
                             if i == retries - 1:
-                                print(
-                                    f"Error occurred while making the API request: {e}")
+                                print(f"Error occurred while making the API request: {e}")
                                 api_comments = []
                             else:
-                                print(
-                                    f"Retrying API request. Attempt {i+1} of {retries}.")
+                                print(f"Retrying API request. Attempt {i+1} of {retries}.")
                                 time.sleep(10)
 
                     # Store response in cache
@@ -102,37 +92,34 @@ for subreddit_name in subreddit_names:
                         'timestamp': time.time()
                     }
 
-                # Initialize wholesome count
-                wholesome_count = 0
-                word_count = collections.Counter()
+                      # Initialize wholesome count
+                    wholesome_count = 0
+                    word_count = collections.Counter()
 
-                # Analyze comments for wholesome words
-                for comment in api_comments:
-                    # Clean and tokenize comment
-                    comment_text = re.sub(
-                        r'[^\w\s]', '', comment['body']).lower()
-                    comment_tokens = comment_text.split()
+                    # Analyze comments for wholesome words
+                    for comment in api_comments:
+                        # Clean and tokenize comment
+                        comment_text = re.sub(r'[^\w\s]', '', comment.body).lower()
+                        comment_tokens = comment_text.split()
 
-                    # Count wholesome occurrences
-                    for token in comment_tokens:
-                        if token in wholesome_set:
-                            wholesome_count += 1
-                            word_count[token] += 1
+                        # Count wholesome occurrences
+                        for token in comment_tokens:
+                            if token in wholesome_set:
+                                wholesome_count += 1
+                                word_count[token] += 1
 
-                # Construct table of wholesome words and counts
-                table_rows = [
-                    f"| {word} | {count} |" for word, count in word_count.items()
-                ]
-                table = "\n".join(table_rows)
+                    # Construct table of wholesome words and counts
+                    table_rows = [
+                        f"| {word} | {count} |" for word, count in word_count.items()
+                    ]
+                    table = "\n".join(table_rows)
 
-                # Construct reply text with wholesome count and table
-                if '!uwucheckself' in incoming_comment.body.lower():
-                    reply_text = f'The number of wholesome occurrences in your recent 500 comments is {wholesome_count}.\n\n| Word | Count |\n| --- | --- |\n{table} |\n\nStay wholesome!  \n\nWanna do something even more wholesome? Leave a \u2B50 at the [GitHub repository](https://github.com/MeowthyVoyager/reddit-wholesome-counter).'
-                else:
-                    reply_text = f'The number of wholesome occurrences in the recent 500 comments of u/{user_name} is {wholesome_count}.\n\n| Word | Count |\n| --- | --- |\n{table} |\n\nStay wholesome!  \n\nWanna do something even more wholesome? Leave a \u2B50 at the [GitHub repository](https://github.com/MeowthyVoyager/reddit-wholesome-counter).'
+                    # Construct reply text with wholesome count and table
+                    if '!uwucheckself' in incoming_comment.body.lower():
+                        reply_text = f'The number of wholesome occurrences in your recent 300 comments is {wholesome_count}.\n\n| Word | Count |\n| --- | --- |\n{table} |\n\nStay wholesome!  \n\nWanna do something even more wholesome? Leave a \u2B50 at the [GitHub repository](https://github.com/MeowthyVoyager/wholesomebot-reddit).'
+                    else:
+                        reply_text = f'The number of wholesome occurrences in the recent 300 comments of u/{user_name} is {wholesome_count}.\n\n| Word | Count |\n| --- | --- |\n{table} |\n\nStay wholesome!  \n\nWanna do something even more wholesome? Leave a \u2B50 at the [GitHub repository](https://github.com/MeowthyVoyager/wholesomebot-reddit).'
 
-                # Add comment to list of comments to reply to
-                comments_to_reply.append((incoming_comment, reply_text))
+                    # Reply to the comment
+                    incoming_comment.reply(reply_text)
 
-                # Call reply() on the comment immediately
-                incoming_comment.reply(reply_text)
